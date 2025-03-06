581f157fd4e822af0b281c5c168be52f165a0934
privacyguard <privacyguard@users.noreply.github.com>
2025-03-06 15:37:12 +0200

    fixes and improvements

--- src/routes/main/(app)/(default-layout)/settings/applications/[id]/AccessManagement.svelte
+++ src/routes/main/(app)/(default-layout)/settings/applications/[id]/AccessManagement.svelte
@@ -1,6 +1,8 @@
 <script>
   import FlexContainer from '$lib/components/common/FlexContainer.svelte';
+  import GridContainer from '$lib/components/common/GridContainer.svelte';
   import Toggle from '$lib/components/common/Toggle.svelte';
+  import AnnouncementIcon from '$lib/components/materialIcons/AnnouncementIcon.svelte';
   import { displayError } from '$lib/modules/errors';
   import { publishOAuthApplication, unpublishOAuthApplication } from '$lib/modules/requests';
 
@@ -8,6 +10,7 @@
   export let loading;
   export let clientId;
   export let published_at;
+  export let isLocalUrl;
   let accessChanging;
 
   async function handlePublicAccessToggle() {
@@ -41,8 +44,14 @@
   <FlexContainer column gap="0.15rem">
     <FlexContainer align_items="center" justify_content="space-between" gap="0.5rem">
       <h5 class="no-margin">Public Access</h5>
-      <Toggle on:click={handlePublicAccessToggle} size="13px" checked={!!published_at} disabled={accessChanging || loading} />
+      <Toggle on:click={handlePublicAccessToggle} size="13px" checked={!!published_at} disabled={accessChanging || loading || isLocalUrl} />
     </FlexContainer>
     <span class="xs">When enabled, any Privacy Portal user would be able to sign in to your application. By default, only you have access.</span>
   </FlexContainer>
+  {#if isLocalUrl}
+    <GridContainer template_columns="18px auto" align_items="center" gap="0.5rem">
+      <AnnouncementIcon color="var(--info-color)" dimension="18px" />
+      <span class="note">Cannot enable Public Access with local domains.</span>
+    </GridContainer>
+  {/if}
 </FlexContainer>
